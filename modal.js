257b24049b039d45ff8ec9ca98e5dca3714088af
venkatesh <venkatesh9ui@gmail.com>
2021-11-08 01:07:28 -0500

    Understanding Named Slots

--- modal.js
+++ modal.js
@@ -43,7 +43,7 @@ class Modal extends HTMLElement {
           padding: 1rem;
         }
 
-        header h1 {
+        ::slotted(h1) {
           font-size: 1.2rem;
         }
 
@@ -65,7 +65,7 @@ class Modal extends HTMLElement {
       <div id='backdrop'></div>
       <div id='modal'>
         <header>
-          <h1>Please Confirm</h1>
+          <slot name='title'>Please Confirm Payment</slot>
         </header>
         <section id='main'>
           <slot></slot>
